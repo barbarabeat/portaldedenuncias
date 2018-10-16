10d3e98d0afd4928b0b970ac34606580f13a9682
Eduardo <edu.coelho96@yahoo.com.br>
2018-10-16 12:20:58 -0300

    Terceiro commit. Exclusão de duas páginas html teste e modificação na index.html.

--- server/server.js
+++ server/server.js
@@ -4,12 +4,28 @@ const path = require("path");
 const port = 8080;
 const bparser = require("body-parser");
 
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+});*/
+
+app.get('/api/:version', function(req, res){
+    res.send(req.params.version);
 });
 
-app.post('/', function(req, res) {
-   res.send(); 
-})
+app.get('/api/users', function(req, res){
+    var user_id = req.param('id');
+    var token = req.param('token');
+    var geo = req.param('geo');
+
+    res.send(user_id + '' + token + '' + geo);
+});
+
+/*app.post('/', function(req, res) {
+    var user_id = req.body.id;
+    var token = req.body.token;
+    var geo = req.body.geo;
+    
+    res.send(user_id + '' + token + '' + geo); 
+});*/
 
 app.listen(port,() => console.log('Portal de Denúncias aberto na porta:' + port));
